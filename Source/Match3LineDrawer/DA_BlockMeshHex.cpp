b425726ba57253fa006e0de13eb9def5ee822af6
Jakov Pavlek <jakov.pavlek@gmail.com>
2021-08-29 05:20:10 +0200

    Add DA_PuzzleHexInfo.uasset in order to expose colored materials and hexagonal stastic mesh. Update map.

--- Source/Match3LineDrawer/DA_BlockMeshHex.cpp
+++ Source/Match3LineDrawer/DA_BlockMeshHex.cpp
@@ -1,4 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "DA_BlockMeshHex.h"
+
+FVector UDA_BlockMeshHex::GetIndexTextRelativeOffset()
+{
+	return IndexTextRelativeOffset;
+}
+
+FVector UDA_BlockMeshHex::GetRelativeScale3D()
+{
+	return RelativeScale3D;
+}
--- Source/Match3LineDrawer/DA_BlockMeshHex.h
+++ Source/Match3LineDrawer/DA_BlockMeshHex.h
@@ -14,10 +14,16 @@ class MATCH3LINEDRAWER_API UDA_BlockMeshHex : public UDataAsset
 {
 	GENERATED_BODY()
 
-		//UPROPERTY(Category = Block, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		//FName Color;
+	UPROPERTY(Category = Block, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* BlockMeshHex = nullptr;
 
-		UPROPERTY(Category = Block, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		class UStaticMeshComponent* BlockMeshHex = nullptr;
+	UPROPERTY(Category = Block, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FVector IndexTextRelativeOffset = FVector(0.f, 0.f, 50.f);
 
+	UPROPERTY(Category = Block, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FVector RelativeScale3D = FVector(2.f, 2.f, 2.f);
+
+public:
+	FVector GetIndexTextRelativeOffset();
+	FVector GetRelativeScale3D();
 };

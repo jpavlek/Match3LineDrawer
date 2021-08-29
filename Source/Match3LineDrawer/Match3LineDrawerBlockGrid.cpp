d6be4c7cf7a234647722851de81bed05510e0a6c
Jakov Pavlek <jakov.pavlek@gmail.com>
2021-08-29 07:18:02 +0200

    Implement game logic; add random color generation function, conversion functions from grid index to 3D coordinates, score function, input handling functions and tile swapping functions.

--- Source/Match3LineDrawer/Match3LineDrawerBlockGrid.cpp
+++ Source/Match3LineDrawer/Match3LineDrawerBlockGrid.cpp
@@ -4,6 +4,7 @@
 #include "Match3LineDrawerBlock.h"
 #include "Components/TextRenderComponent.h"
 #include "Engine/World.h"
+#include "Math/IntPoint.h"
 
 #define LOCTEXT_NAMESPACE "PuzzleBlockGrid"
 
@@ -16,11 +17,10 @@ AMatch3LineDrawerBlockGrid::AMatch3LineDrawerBlockGrid()
 	// Create static mesh component
 	ScoreText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("ScoreText0"));
 	ScoreText->SetRelativeLocation(FVector(600.f, -200.f, 0.f));
-	ScoreText->SetRelativeRotation(FRotator(90.f,0.f,0.f));
+	ScoreText->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
 	ScoreText->SetText(FText::Format(LOCTEXT("ScoreFmt", "Score: {0}"), FText::AsNumber(0)));
 	ScoreText->SetupAttachment(DummyRoot);
 }
-
 
 void AMatch3LineDrawerBlockGrid::BeginPlay()
 {
@@ -30,33 +30,208 @@ void AMatch3LineDrawerBlockGrid::BeginPlay()
 	const int32 NumBlocks = SizeHorizontal * SizeVertical;
 
 	// Loop to spawn each block
-	for(int32 BlockIndex=0; BlockIndex<NumBlocks; BlockIndex++)
+	for (int32 BlockIndex = 0; BlockIndex < NumBlocks; BlockIndex++)
 	{
-		const float XOffset = (BlockIndex / SizeHorizontal) * BlockSpacingHorizontal + (BlockIndex % SizeHorizontal % 2) * BlockSpacingHorizontal / 2.0f; // Divide by dimension
-		const float YOffset = (BlockIndex % SizeHorizontal) * BlockSpacingVertical; // Modulo gives remainder
-		// Make position vector, offset from Grid location
-		const FVector BlockLocation = FVector(XOffset, YOffset, 0.f) + GetActorLocation() + GridOffset;
+		const FVector BlockLocation = IndexTo3DCoordinate(BlockIndex);
 
 		// Spawn a block
 		AMatch3LineDrawerBlock* BlockToAdd = GetWorld()->SpawnActor<AMatch3LineDrawerBlock>(BlockLocation, FRotator(0, 0, 0));
-		BlockToAdd->SelectRandomMaterial();
+		BlockToAdd->SelectRandomColor();
+		BlockToAdd->UpdateMaterial();
+		BlockToAdd->SetIndex(BlockIndex);
 
 		// Tell the block about its owner
 		if (BlockToAdd != nullptr)
 		{
 			BlockToAdd->OwningGrid = this;
 		}
+		Tiles.Add(BlockIndex, BlockToAdd);
 	}
 }
 
-
 void AMatch3LineDrawerBlockGrid::AddScore()
 {
-	// Increment score
-	Score++;
+	AddScore(1);
+}
+
+void AMatch3LineDrawerBlockGrid::AddScore(int32 Amount)
+{
+	Score += Amount;
 
 	// Update text
 	ScoreText->SetText(FText::Format(LOCTEXT("ScoreFmt", "Score: {0}"), FText::AsNumber(Score)));
+}
+
+FIntPoint AMatch3LineDrawerBlockGrid::IndexToGridCoordinate(int32 index) const
+{
+	int32 row = index / SizeHorizontal;
+	int32 column = index % SizeHorizontal;
+	return FIntPoint(row, column);
+}
+
+FVector2D AMatch3LineDrawerBlockGrid::GridCoordinateToOffset(const FIntPoint& position) const
+{
+	const FVector2D result(position.X * BlockSpacingHorizontal + (position.Y % 2) * BlockSpacingHorizontal / 2.0f, // Divide by dimension
+		position.Y * BlockSpacingVertical); // Modulo gives remainder
+	return result;
+}
+
+FVector AMatch3LineDrawerBlockGrid::IndexToCoordinate(int32 index) const
+{
+	int32 row = index / SizeHorizontal;
+	int32 column = index % SizeHorizontal;
+	return FVector(row, column, 0.0f);
+}
+
+int32 AMatch3LineDrawerBlockGrid::GridCoordinateToIndex(const FIntPoint& Position) const
+{
+	int32 index = Position.X * SizeHorizontal + Position.Y;
+	return index;
+}
+
+FVector AMatch3LineDrawerBlockGrid::OffsetTo3DCoordinate(const FVector2D& RelativeOffset) const
+{
+	return GetActorLocation() + GridOffset + FVector(RelativeOffset.X, RelativeOffset.Y, 0.f);
+}
+
+FVector AMatch3LineDrawerBlockGrid::IndexTo3DCoordinate(int32 BlockIndex) const
+{
+	const FIntPoint position = IndexToGridCoordinate(BlockIndex);
+	const FVector2D relativeOffset = GridCoordinateToOffset(position);
+	const FVector BlockLocation = OffsetTo3DCoordinate(relativeOffset);
+	return BlockLocation;
+}
+
+int32 AMatch3LineDrawerBlockGrid::GetNumberOfSelectedTiles()
+{
+	return NumberOfSelectedTiles;
+}
+
+AMatch3LineDrawerBlock* AMatch3LineDrawerBlockGrid::GetTile(int32 Index)
+{
+	if (Index == -1)
+	{
+		return nullptr;
+	}
+
+	return Tiles[Index];
+}
+
+void AMatch3LineDrawerBlockGrid::DeselectAllTiles()
+{
+	UE_LOG(LogTemp, Log, TEXT("DeselectAllTiles"));
+	TArray<int32> indices;
+	SelectedTiles.GetKeys(indices);
+	for (int32 index : indices)
+	{
+		if (AMatch3LineDrawerBlock* block = GetTile(index))
+		{
+			block->DeselectBlock();
+		}
+	}
+	SelectedTiles.Empty();
+	SelectedTiles.Compact();
+	LastSelectedBlockIndex = -1;
+
+}
+
+void AMatch3LineDrawerBlockGrid::ShowSelectedTiles(bool ShouldShow)
+{
+	TArray<int32> indices;
+	SelectedTiles.GetKeys(indices);
+	for (int32 index : indices)
+	{
+		if (AMatch3LineDrawerBlock* block = GetTile(index))
+		{
+			block->SetActorHiddenInGame(!ShouldShow);
+		}
+	}
+}
+
+void AMatch3LineDrawerBlockGrid::RefreshColors()
+{
+	TArray<int32> indices;
+	SelectedTiles.GetKeys(indices);
+	for (int32 index : indices)
+	{
+		if (AMatch3LineDrawerBlock* block = GetTile(index))
+		{
+			block->UpdateMaterial();
+		}
+	}
+}
+
+void AMatch3LineDrawerBlockGrid::RandomizeSelectedTiles()
+{
+	TArray<int32> indices;
+	SelectedTiles.GetKeys(indices);
+	for (int32 index : indices)
+	{
+		if (AMatch3LineDrawerBlock* block = GetTile(index))
+		{
+			block->SelectRandomColor();
+			block->UpdateMaterial();
+		}
+	}
+}
+
+bool AMatch3LineDrawerBlockGrid::SwapTiles(int32 TileIndex0, int32 TileIndex1)
+{
+	TArray<int32> selectedIndices;
+	SelectedTiles.GetKeys(selectedIndices);
+	bool isSelectedIndex0 = selectedIndices.Contains(TileIndex0);
+	bool isSelectedIndex1 = selectedIndices.Contains(TileIndex1);
+	if (isSelectedIndex0 == isSelectedIndex1)
+	{
+		return false;
+	}
+
+	int32 selectedIndex = TileIndex0;
+	int32 otherIndex = TileIndex1;
+	if (isSelectedIndex1)
+	{
+		selectedIndex = TileIndex1;
+		otherIndex = TileIndex0;
+	}
+
+	AMatch3LineDrawerBlock* selectedBlock = GetTile(selectedIndex);
+	AMatch3LineDrawerBlock* otherBlock = GetTile(otherIndex);
+
+	selectedBlock->SetIndex(otherIndex);
+	otherBlock->SetIndex(selectedIndex);
+
+	selectedBlock->SetActorLocation(IndexTo3DCoordinate(otherIndex));
+	otherBlock->SetActorLocation(IndexTo3DCoordinate(selectedIndex));
+
+	Tiles[selectedIndex] = otherBlock;
+	Tiles[otherIndex] = selectedBlock;
+
+	SelectedTiles.Remove(selectedIndex);
+	SelectedTiles.Add(otherIndex);
+	return true;
+}
+
+void AMatch3LineDrawerBlockGrid::SwapSelectedTiles()
+{
+	int32 TotalSize = SizeHorizontal * SizeVertical;
+	bool swapped = false;
+	do
+	{
+		TArray<int32> indices;
+		SelectedTiles.GetKeys(indices);
+		indices.Sort([](const int32 a, const int32 b) { return a > b; });
+		swapped = false;
+		for (int32 index : indices)
+		{
+			int32 otherIndex = index + SizeHorizontal;
+			if (otherIndex >= TotalSize)
+			{
+				continue;
+			}
+			swapped = swapped || SwapTiles(index, index + SizeHorizontal);
+		}
+
+	} while (swapped);
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/Match3LineDrawer/Match3LineDrawerBlockGrid.h
+++ Source/Match3LineDrawer/Match3LineDrawerBlockGrid.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Match3LineDrawerBlock.h"
 #include "Match3LineDrawerBlockGrid.generated.h"
 
 /** Class used to spawn blocks and manage score */
@@ -23,26 +24,36 @@ class AMatch3LineDrawerBlockGrid : public AActor
 public:
 	AMatch3LineDrawerBlockGrid();
 
-	/** How many blocks have been clicked */
 	int32 Score;
 
-	/** Number of blocks along each side of grid */
+	/** Index of last selected block */
+	int32 LastSelectedBlockIndex = -1;
+	
+	/** Number of currently selected block tiles */
+	int32 NumberOfSelectedTiles = 0;
+	
+	/** Number of blocks along horizontal side of grid */
 	UPROPERTY(Category=Grid, EditAnywhere, BlueprintReadOnly)
 	int32 SizeHorizontal = 7;
 
-	/** Number of blocks along each side of grid */
+	/** Number of blocks along vertical side of grid */
 	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadOnly)
 	int32 SizeVertical = 6;
 	
+	/** Horizontal spacing between blocks */
 	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadOnly)
 	float BlockSpacingHorizontal = 300.0f;
 	
+	/** Vertical spacing between blocks */
 	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadOnly)
 	float BlockSpacingVertical = 220.0f;
 
-	/** Spacing of blocks */
+	/** Offset of the whole grid relative to the screen */
 	UPROPERTY(Category=Grid, EditAnywhere, BlueprintReadOnly)
 	FVector GridOffset = FVector(300.0f, 300, 0.0f);
+
+	UPROPERTY(Category = Grid, VisibleDefaultsOnly, BlueprintReadOnly)
+	bool SelectionEnabled = false;
 
 protected:
 	// Begin AActor interface
@@ -54,10 +65,49 @@ public:
 	/** Handle the block being clicked */
 	void AddScore();
 
+	void AddScore(int32 Amount);
+
 	/** Returns DummyRoot subobject **/
 	FORCEINLINE class USceneComponent* GetDummyRoot() const { return DummyRoot; }
 	/** Returns ScoreText subobject **/
 	FORCEINLINE class UTextRenderComponent* GetScoreText() const { return ScoreText; }
+
+	/** Calculates grid coordinates (row, column) from index **/
+	FIntPoint IndexToGridCoordinate(int32 index) const;
+
+	FVector2D GridCoordinateToOffset(const FIntPoint& position) const;
+
+	FVector IndexToCoordinate(int32 index) const;
+
+	int32 GridCoordinateToIndex(const FIntPoint& Position) const;
+
+	FVector OffsetTo3DCoordinate(const FVector2D& RelativeOffset) const;
+
+	FVector IndexTo3DCoordinate(int32 BlockIndex) const;
+
+	int32 GetNumberOfSelectedTiles();
+
+	AMatch3LineDrawerBlock* GetTile(int32 Index);
+
+	void DeselectAllTiles();
+
+	void ShowSelectedTiles(bool ShouldShow = true);
+
+	void RefreshColors();
+
+	void RandomizeSelectedTiles();
+
+	bool SwapTiles(int32 TileIndex0, int32 TileIndex1);
+
+	void SwapSelectedTiles();
+
+	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadOnly)
+	TMap<int32, AMatch3LineDrawerBlock*> Tiles;
+
+	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadOnly)
+	TMap<int32, int32> SelectedTiles;
+
+	void HideBlock();
 };
 
 
